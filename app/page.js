e04dc0e6223a50928b09486e3a870183f46f73ec
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-09-12 23:01:32 +0530

    us component and videos

--- app/page.js
+++ app/page.js
@@ -2,11 +2,15 @@ import React from "react";
 import Carousel from "@/components/Home/Carousel";
 import Gallery from "@/components/Home/Gallery";
 import { Home } from "@/components/Home/data"
+import Us from "@/components/Home/Us";
+import Videos from "@/components/Home/Videos";
 
 const HomePage = () => {
   return (
     <>
       <Carousel data={Home.carousel} heading={Home.heading} />
+      <Us/>
+      <Videos/>
       <Gallery data={Home.gallery} />
     </>
   );
