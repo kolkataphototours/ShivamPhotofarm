787b846761484f5bab212618e0efce9f4639a16a
r1tikpatil <ritikpatil566@gmail.com>
2023-09-22 14:08:00 +0530

    added gallery

--- components/Home/Gallery.jsx
+++ components/Home/Gallery.jsx
@@ -1,50 +1,60 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 
 const Gallery = ({ data }) => {
-    return (
-        <div className='relative -z-20 flex bg-gray-100 py-16 '>
-            <div className='w-1/3 '>
-                <div className='p-1 pl-2 h-1/3 '>
-                    <img key={data[0].id} src={data[0].url} alt={"picture"} className='h-full w-full rounded-b-lg shadow-md' />
-                </div>
-                <div className='p-1 pl-2 h-1/3'>
-                    <img key={data[1].id} src={data[1].url} alt={"picture"} className='h-full w-full rounded-lg shadow-md' />
-                </div>
-                <div className='p-1 pl-2 h-1/3'>
-                    <img key={data[2].id} src={data[2].url} alt={"picture"} className='h-full w-full rounded-t-lg shadow-md' />
-                </div>
-            </div>
+  return (
+    <div className="relative -z-20 my-4 w-screen flex items-center flex-col">
+      <div className="w-[88%] md:w-[75%] md:h-screen">
+        <img src={data[0].url} alt="img" className="h-full w-full" />
+      </div>
 
-            <div className='w-1/3'>
-                <div className='p-1 h-1/4'>
-                    <img key={data[3].id} src={data[3].url} alt={"picture"} className='h-full w-full rounded-b-lg shadow-md' />
-                </div>
-                <div className='p-1 h-1/4'>
-                    <img key={data[4].id} src={data[4].url} alt={"picture"} className='h-full w-full rounded-lg shadow-md' />
-                </div>
-                <div className='p-1 h-1/4'>
-                    <img key={data[5].id} src={data[5].url} alt={"picture"} className='h-full w-full rounded-lg shadow-md' />
-                </div>
-                <div className='p-1 h-1/4'>
-                    <img key={data[6].id} src={data[6].url} alt={"picture"} className='h-full w-full rounded-t-lg shadow-md' />
-                </div>
-            </div>
+      <div className="w-[88%] md:w-[75%] md:h-screen mt-3">
+        <img src={data[1].url} alt="img" className="h-full w-full" />
+      </div>
 
-            <div className='w-1/3'>
-                <div className='p-1 pr-2 h-1/3'>
-                    <img key={data[7].id} src={data[7].url} alt={"picture"} className='h-full w-full rounded-b-lg shadow-md' />
-                </div>
-                <div className='p-1 pr-2 h-1/3'>
-                    <img key={data[8].id} src={data[8].url} alt={"picture"} className='h-full w-full rounded-lg shadow-md' />
-                </div>
-                <div className='p-1 pr-2 h-1/3'>
-                    <img key={data[9].id} src={data[9].url} alt={"picture"} className='h-full w-full rounded-t-lg shadow-md' />
-                </div>
-            </div>
-
+      <div className="w-[88%] h-auto md:w-[75%] mt-3 md:h-[63vh] flex flex-col md:flex-row md:justify-between">
+        <div className="w-full md:w-[69%] md:h-full">
+          <img src={data[3].url} alt="img" className="h-full w-full" />
         </div>
-    )
-}
+        <div className="mt-3 w-full h-[60vh] md:w-[30%] md:h-full md:mt-0">
+          <img src={data[4].url} alt="img" className="h-full w-full" />
+        </div>
+      </div>
+
+      <div className="w-[88%] md:w-[75%] mt-3 md:h-[50vh] flex flex-col md:flex-row md:justify-between">
+        <div className="w-full h-[60vh] md:w-[20%] md:h-full">
+          <img src={data[5].url} alt="img" className="h-full w-full" />
+        </div>
+        <div className="w-full h-[60vh] mt-3 md:mt-0 md:w-[20%] md:h-full ">
+          <img src={data[6].url} alt="img" className="h-full w-full" />
+        </div>
+        <div className="md:w-[58%] md:h-full mt-3 md:mt-0">
+          <img src={data[7].url} alt="img" className="h-full w-full" />
+        </div>
+      </div>
+
+      <div className="w-[88%] md:w-[75%] mt-3 md:h-[63vh] flex flex-col md:flex-row md:justify-between">
+        <div className="md:w-[69%] md:h-full">
+          <img src={data[8].url} alt="img" className="h-full w-full" />
+        </div>
+        <div className="w-full h-[60vh] md:w-[30%] md:h-full mt-3 md:mt-0">
+          <img src={data[9].url} alt="img" className="h-full w-full" />
+        </div>
+      </div>
+
+      <div className="w-[88%] md:w-[75%] mt-3 md:h-[50vh] flex flex-col md:flex-row md:justify-between">
+        <div className="w-full h-[60vh] md:w-[20%] md:h-full">
+          <img src={data[5].url} alt="img" className="h-full w-full" />
+        </div>
+        <div className="w-full h-[60vh] mt-3 md:mt-0 md:w-[20%] md:h-full ">
+          <img src={data[6].url} alt="img" className="h-full w-full" />
+        </div>
+        <div className="mt-3 md:mt-0 md:w-[58%] md:h-full ">
+          <img src={data[7].url} alt="img" className="h-full w-full" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Gallery;
