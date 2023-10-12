3c139237c0b7d2787a86c495a1cfdee5889bc83b
r1tikpatil <ritikpatil566@gmail.com>
2023-10-12 11:56:02 +0530

    did portraits responsive

--- app/works/portraits/page.jsx
+++ app/works/portraits/page.jsx
@@ -9,23 +9,23 @@ const Portraits = () => {
         const { images } = data;
         return (
           <div className="mt-2">
-            <div className="md:w-[75%] h-[150vh] mx-auto flex flex-row justify-between">
-              <div className="w-[50%] h-full">
+            <div className="w-[90%] md:w-[75%] h-[200vh] md:h-[150vh] mx-auto flex flex-col md:flex-row justify-between">
+              <div className="w-[100%] md:w-[50%] h-[50%] md:h-full">
                 <img src={images[0]} alt="img" className="m-1 h-[65%] w-full" />
                 <img src={images[1]} alt="img" className="m-1 h-[35%] w-full" />
               </div>
-              <div className="w-[49.5%] h-full">
+              <div className="w-[100%] mt-2 md:mt-0 md:w-[49.5%] h-[50%] md:h-full">
                 <img src={images[2]} alt="img" className="m-1 h-[35%] w-full" />
                 <img src={images[3]} alt="img" className="m-1 h-[65%] w-full" />
               </div>
             </div>
 
-            <div className="mt-2 md:w-[75%] h-[150vh] mx-auto flex flex-row justify-between">
-              <div className="w-[50%] h-full">
+            <div className="mt-2 w-[90%] md:w-[75%] h-[200vh] md:h-[150vh] mx-auto flex flex-col md:flex-row justify-between">
+              <div className="w-[100%] md:w-[50%] h-[50%] md:h-full">
                 <img src={images[4]} alt="img" className="m-1 h-[35%] w-full" />
                 <img src={images[5]} alt="img" className="m-1 h-[65%] w-full" />
               </div>
-              <div className="w-[49.5%] h-full">
+              <div className="w-[100%] mt-2 md:mt-0 md:w-[49.5%] h-[50%] md:h-full">
                 <img src={images[6]} alt="img" className="m-1 h-[65%] w-full" />
                 <img src={images[7]} alt="img" className="m-1 h-[35%] w-full" />
               </div>
